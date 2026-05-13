678b9bc9f566a9293b0352eaad3fe49d0132b59d
hay46 <haymanotebabu2@gmail.com>
2026-05-13 12:35:32 +0300

    the backend dependencey install

---
+++ server/Server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+const PORT = 3000;
+app.listen(PORT, (err) => {
+    if (err) {
+        console.error('Error starting server:', err);
+        return;
+    }
+  console.log(`Server is running on port ${PORT}`);
+});
