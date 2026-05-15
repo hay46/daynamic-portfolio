2957a7ce180990b985931f34e01e73a1273f4aad
hay46 <haymanotebabu2@gmail.com>
2026-05-16 02:03:26 +0300

    the controller of the user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 import db from '../config/db.js'
 import { userFindbyEmail } from '../models/userModel.js';
 
-export const loginUser = (req, res) => {
+export const loginUser = async (req, res) => {
     const { email, password } = req.body;
 
     userFindbyEmail(email, async (err, results) => {
@@ -23,7 +23,37 @@ export const loginUser = (req, res) => {
         process.env.JWT_SECRET,
         { expiresIn: '1h' }
         );
-        res.json({ token });
+        res.status(200).json({ 
+            message: 'Login seccessful',
+        
+            token,
+            user: {
+                id: user.id,
+                email:user.email,
+                name: user.name
+            }
+         });
     });
 };
+export const registerUser = async (req, res) => {
+ const [name,email, password] = req.body;
 
+ // hashed password
+
+ const hashedPassword = await bycrpt.hash(password,10);
+ const sql = `INSRE INTO USERS (name,email,password) VALUES (?,?,?)`
+
+ db.query(sql,{name,email,hashedPassword},(err,resault)=>{
+
+    if(err){
+        res.status(400).json({
+            message: 'users already exists'
+        });
+    }else{
+        res.status(200).json({
+            message: 'users created secssessfuly'
+        })  ;  
+    }
+ })
+
+ };
