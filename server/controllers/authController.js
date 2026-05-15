f3e2f90ffb9cca7affb65a9d1e66ee58bd67e183
hay46 <haymanotebabu2@gmail.com>
2026-05-16 00:11:18 +0300

    the auth contraoller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,5 +6,24 @@ import { userFindbyEmail } from '../models/userModel.js';
 export const loginUser = (req, res) => {
     const { email, password } = req.body;
 
-    userFindbyEmail(email, (err, results) => {
+    userFindbyEmail(email, async (err, results) => {
         if (err) {
+          res.status(500).json({ message: 'Database error' });
+        }
+        const user = results[0];
+        const isPasswordValid = await bcrpt.compare(password, user.password);
+        if (!isPasswordValid) {
+            res.status(401).json({ message: 'Invalid credentials' });
+        } 
+        const tolken = jwt.sign(
+            {
+            id: user.id,
+            email: user.email
+            },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+        );
+        res.json({ token });
+    });
+};
+
