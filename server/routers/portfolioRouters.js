cab7f3b2db0fb6fb0d5e91c1162f4f5f2487b527
hay46 <haymanotebabu2@gmail.com>
2026-05-19 18:03:35 +0300

    the delete error fix

--- server/routers/portfolioRouters.js
+++ server/routers/portfolioRouters.js
@@ -2,22 +2,22 @@ import {getAlldata,getById,addInformation,edit_portfolio,deletData} from '../con
 import express from 'express';
 import {verfidata} from '../middlewares/authMiddlewar.js';
 
-const Router =express.Router;
+const router =express.Router();
 
 //public user
 
-Router.get("/",getAlldata);
+router.get("/",getAlldata);
 
-Router.get("/:id",getById);
+router.get("/:id",getById);
 
 //admin user also view
 //addinformation router
-Router.post("/",verfidata,addInformation);
+router.post("/",verfidata,addInformation);
 //update information router
-Router.put("/:id",verfidata,edit_portfolio,);
+router.put("/:id",verfidata,edit_portfolio,);
 
 //delate user by id;
 
-Router.delet("/:id",verfidata,deletData);
+router.delete("/:id", verfidata, deletData);
 
 export default router;
