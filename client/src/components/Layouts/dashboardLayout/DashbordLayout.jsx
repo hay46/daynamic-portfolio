eadb1664cc117a9ea00eac3f13d06013a218e6e0
hay46 <haymanotebabu2@gmail.com>
2026-05-30 08:16:44 +0300

    the dashbord layout page

---
+++ client/src/components/Layouts/dashboardLayout/DashbordLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {outlet} from 'react-router-dom'
+import sidebare from '../../admin/dashbord/sidbar/Sidebar'
+import Topbar from '../../admin/dashbord/topbar/Topbar'
+import style from './DashbordLayout.module.css'
+const DashbordLayout = () => {
+  return (
+    <div>DashbordLayout</div>
+  )
+}
+
+export default DashbordLayout
