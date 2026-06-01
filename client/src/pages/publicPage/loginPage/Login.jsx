255b617de9faa438e1048703d536d38a38cf80d9
hay46 <haymanotebabu2@gmail.com>
2026-06-01 09:18:19 +0300

    the login page

--- client/src/pages/publicPage/loginPage/Login.jsx
+++ client/src/pages/publicPage/loginPage/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../../context/AuthContext'; // ✅ fixed path & name
+import { useAuth } from '../../../../src/contaxt/AuthContaxt'; 
 import styles from './Login.module.css';
 
 const Login = () => {
