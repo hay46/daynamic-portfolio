4703f35e77f694b9622ecae15fe48e6d6ab36070
haymanot ebabu <haymanotebabu2@gmail.com>
2026-09-14 11:18:15 +0300

    the contact form .module.css update

--- server/server.js
+++ server/server.js
@@ -7,7 +7,9 @@ import portfolioRouters from "./routers/portfolioRouters.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://daynamic-portfolio-mbuq.vercel.app"
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRouters);
